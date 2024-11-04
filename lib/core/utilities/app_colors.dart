ae2fe2250b98ee15c30d4ac4bd04056a159a6d3a
ali72-20 <as20201118@gmail.com>
2024-11-05 00:45:48 +0200

    add: car item card decoration

--- lib/core/utilities/app_colors.dart
+++ lib/core/utilities/app_colors.dart
@@ -6,4 +6,6 @@ abstract class AppColors{
    static const Color gray = Colors.grey;
    static const Color black = Colors.black;
    static const Color onBoarding = Color(0xff2c2b34);
+   static const Color carCard = Color(0xffF3F3F3);
+   static const Color cardBorder = Colors.black12;
 }
--- lib/presentation/widgets/car_item_card.dart
+++ lib/presentation/widgets/car_item_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rent_car/core/utilities/app_colors.dart';
 
 import '../../core/utilities/app_images.dart';
 import '../../core/utilities/styles.dart';
@@ -13,6 +15,18 @@ class CarItemCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       padding: const EdgeInsets.all(20),
+
+      decoration: BoxDecoration(
+        color: AppColors.carCard,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow:  const [
+          BoxShadow(
+            color: AppColors.cardBorder,
+            blurRadius: 10,
+            spreadRadius: 5
+          )
+        ]
+      ),
       child: Column(
         children: [
           Image.asset(AppImages.carImage),
@@ -24,21 +38,19 @@ class CarItemCard extends StatelessWidget {
             height: 10,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  Image.asset(AppImages.gps),
+                  Text("${car.distance!.toStringAsFixed(0)} KM"),
                   Image.asset(AppImages.pump),
                   Text("${car.fuelCapacity!.toStringAsFixed(0)} L")
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset(AppImages.gps),
-                  Text("${car.distance!.toStringAsFixed(0)} KM")
-                ],
-              )
+              Text("\$${car.pricePerHour!.toStringAsFixed(2)} /h",style:
+                Styles.style16.copyWith(color: AppColors.black),),
             ],
           )
         ],
