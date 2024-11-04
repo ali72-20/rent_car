da31689e4845fac7d9edb4640dac998b10d0a444
ali72-20 <as20201118@gmail.com>
2024-11-04 22:31:23 +0200

    add:bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rent_car/core/app_routes.dart';
 import 'package:rent_car/core/di/di.dart';
+import 'package:rent_car/core/simple_observer.dart';
 import 'package:rent_car/core/utilities/localization/l10n.dart';
 import 'package:rent_car/firebase_options.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:rent_car/presentation/pages/on_boarding.dart';
 
 void main() async {
   configureDependencies();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
 
