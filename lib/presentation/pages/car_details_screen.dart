a4e3c917d47a6e71f77e2d773a5361db094dbf6f
ali72-20 <as20201118@gmail.com>
2024-11-05 02:05:08 +0200

    add: car details screen title

--- lib/presentation/pages/car_details_screen.dart
+++ lib/presentation/pages/car_details_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:rent_car/core/utilities/styles.dart';
 import 'package:rent_car/presentation/widgets/map_card.dart';
 import 'package:rent_car/presentation/widgets/more_cards.dart';
 import 'package:rent_car/presentation/widgets/user_card.dart';
 import '../../domain/entities/car_entity.dart';
 import '../widgets/car_details_card.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CarDetailsScreen extends StatelessWidget {
   const CarDetailsScreen({super.key});
@@ -12,9 +14,10 @@ class CarDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const ListTile(
-          leading: Icon(Icons.arrow_back_ios_new),
-          trailing: Icon(Icons.info_outline),
+        title:  ListTile(
+          leading: const Icon(Icons.arrow_back_ios_new),
+          title: Text(AppLocalizations.of(context)!.information, style: Styles.style20, textAlign: TextAlign.center,),
+          trailing: const Icon(Icons.info_outline),
         ),
       ),
       body: Column(
